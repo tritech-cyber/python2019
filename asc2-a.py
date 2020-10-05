76e318728a4367675f1fa2824daf79a57721891f
Tri-Tech <craig.coleman@ksd.org>
2020-10-05 09:51:05 -0700

    Add files via upload

---
+++ asc2-a.py
@@ -0,0 +1,24 @@
+#asc2-a.py cwc
+#divertido 
+#This code will get saved to github.com/username/python repository
+# Please start typing this code
+# Variables
+i1 = 33  #integer 1
+i2 = 48
+i3 = 65
+i4 = 65+25
+i5 = 97
+#output
+print("Integers")
+print(i1,i2,i3,i4,i5)
+# Type cast
+c1 = chr(i1)
+c2 = chr(i2)
+c3 = chr(i3)
+c4 = chr(i4)
+c5 = chr(i5)
+print("Characters")
+print(c1,c2,c3,c4,c5)
+'''
+https://github.com/tritechsc?tab=repositories
+'''
